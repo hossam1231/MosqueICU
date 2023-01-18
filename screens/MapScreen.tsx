df131d27203c228916f0a38081c9ea44e7648eb7
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-01-18 00:47:33 +0000

    push

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -101,6 +101,7 @@ import {
 } from "native-base";
 import { MapTopBar } from "../components/Map";
 import { Ionicons } from "@expo/vector-icons";
+import FlatListFL from "../components/FlatList.Map";
 
 const MapScreen = () => {
   // ref
@@ -133,54 +134,9 @@ const MapScreen = () => {
         onChange={handleSheetChanges}
       >
         <View style={styles.contentContainer}>
-          {/* <Box
-            bg="primary.600"
-            py="4"
-            px="3"
-            borderRadius="5"
-            rounded="md"
-            width={375}
-            maxWidth="100%"
-          >
-            <HStack justifyContent="space-between">
-              <Box justifyContent="space-between">
-                <VStack space="2">
-                  <Text fontSize="sm" color="white">
-                    Today @ 9PM
-                  </Text>
-                  <Text color="white" fontSize="xl">
-                    Let's talk about avatar!
-                  </Text>
-                </VStack>
-                <Pressable
-                  rounded="xs"
-                  bg="primary.400"
-                  alignSelf="flex-start"
-                  py="1"
-                  px="3"
-                >
-                  <Text
-                    textTransform="uppercase"
-                    fontSize="sm"
-                    fontWeight="bold"
-                    color="white"
-                  >
-                    Remind me
-                  </Text>
-                </Pressable>
-              </Box>
-              <Image
-                source={{
-                  uri: "https://media.vanityfair.com/photos/5ba12e6d42b9d16f4545aa19/3:2/w_1998,h_1332,c_limit/t-Avatar-The-Last-Airbender-Live-Action.jpg",
-                }}
-                alt="Aang flying and surrounded by clouds"
-                height="100"
-                rounded="full"
-                width="100"
-              />
-            </HStack>
-          </Box> */}
           <HStack
+            my="2"
+            mb="5"
             alignItems={"center"}
             justifyContent="center"
             space={3}
@@ -214,6 +170,7 @@ const MapScreen = () => {
               <Avatar.Badge bg="green.500" />
             </Avatar>
           </HStack>
+          <FlatListFL />
         </View>
       </BottomSheet>
     </View>
