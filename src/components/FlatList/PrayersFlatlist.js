cb9e7a7238570e48aadaa048bd45f358c011a468
Aamir Adat <74106642+amdat99@users.noreply.github.com>
2023-02-08 19:21:34 +0000

    fix: remove show showsHorizontalScrollIndicator for android support

--- src/components/FlatList/PrayersFlatlist.js
+++ src/components/FlatList/PrayersFlatlist.js
@@ -1,16 +1,5 @@
 import React from "react";
-import {
-  Box,
-  FlatList,
-  Heading,
-  Avatar,
-  HStack,
-  VStack,
-  Text,
-  Spacer,
-  Center,
-  NativeBaseProvider,
-} from "native-base";
+import { Box, FlatList, Heading, Avatar, HStack, VStack, Text, Spacer, Center, NativeBaseProvider } from "native-base";
 import AnimationWithImperativeApi from "components/LottieAnimation";
 
 export default function PrayersFlatlist() {
@@ -19,15 +8,13 @@ export default function PrayersFlatlist() {
       id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
       fullName: "Fajir",
 
-      avatarUrl:
-        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     },
     {
       id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
       fullName: "Duhur",
 
-      avatarUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU",
+      avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU",
     },
     {
       id: "58694a0f-3da1-471f-bd96-145571e29d72",
@@ -39,22 +26,19 @@ export default function PrayersFlatlist() {
       id: "68694a0f-3da1-431f-bd56-142371e29d72",
       fullName: "Magrib",
 
-      avatarUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU",
+      avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU",
     },
     {
       id: "28694a0f-3da1-471f-bd96-142456e29d72",
       fullName: "Ishah",
 
-      avatarUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU",
+      avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU",
     },
   ];
   return (
     <Box>
       <FlatList
         horizontal
-        showsHorizontalScrollIndicator="false"
         data={data}
         renderItem={({ item }) => (
           //   <VStack space={3} m="2">
